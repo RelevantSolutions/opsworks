c148144a4f141d901cfb11b539faecea6d22e507
Justin Imhoff <justin@justinimhoff.com>
2015-01-27 11:45:12 -0700

    Update

--- metadata.rb
+++
@@ -1,6 +0,0 @@
-name "opsworks"
-maintainer "justin"
-description "Custom tomcat deploy"
-version "0.3"
-
-recipe "opsworks::tomcat", "Write custom configuration and notify rails application to restart upon changes."
