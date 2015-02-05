c27464a0b05db2fb43670441a1cf83a5099c9d9f
Justin Imhoff <justin@justinimhoff.com>
2015-02-05 16:47:54 -0700

    Update

--- itis/metadata.rb
+++ itis/metadata.rb
@@ -4,5 +4,4 @@ maintainer  "AWS OpsWorks"
 license     "Apache 2.0"
 version     "1.0.0"
 
-recipe "itis::default", "Default deploy"
-recipe "itis::java", "Deploy a Java application"
+recipe "itis::permissions", "permissions"
