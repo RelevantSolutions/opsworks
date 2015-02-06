40356b661abeeb9de94f2e4dbceba11c1b465160
Justin Imhoff <justin@justinimhoff.com>
2015-02-06 15:12:01 -0700

    Update

--- itis/recipes/permissions.rb
+++ itis/recipes/permissions.rb
@@ -1,6 +1,6 @@
 node[:deploy].each do |application, deploy|
     Chef::Log.debug("Updating permissions: #{application}")
     app_root = "#{deploy[:deploy_to]}/current"
-    execute "chown -R www-data #{app_root}" do
+    execute "chmod -R a+rwx #{app_root}" do
     end
 end
