766b3d620d84f415c3be33ff7605b3ef561b0209
Justin Imhoff <justin@justinimhoff.com>
2015-02-06 13:13:27 -0700

    Update sudo

--- itis/metadata.rb
+++ itis/metadata.rb
@@ -4,4 +4,5 @@ maintainer  "AWS OpsWorks"
 license     "Apache 2.0"
 version     "1.0.0"
 
-recipe "itis::permissions", "permissions"
+recipe "itis::permissions", "Set correct permissions to deployed app"
+recipe 'itis::sudo', 'Installs sudo and configures /etc/sudoers'
